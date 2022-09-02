7bffd16a32ea94f37737d88549bb144f04f4a5a4
yuhannes <112183271+yuhannes@users.noreply.github.com>
2022-09-02 09:27:10 +0300

    ULAK SYSTEM

---
+++ ulaksystem/scripts/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# APT
+echo "UBUNTU APT"
+apt-get update
+
+# RSYNC
+echo "RSYNC"
+apt-get install rsync
+
+# CURL
+echo "CURL"
+apt-get install curl
+
+# TEST FILES
+echo "TEST FILES"
+mkdir data
+cd data/
+curl http://ipv4.download.thinkbroadband.com/20MB.zip --output 20MB.zip
+curl http://ipv4.download.thinkbroadband.com/50MB.zip --output 50MB.zip
+curl http://ipv4.download.thinkbroadband.com/100MB.zip --output 100MB.zip
+curl https://speed.hetzner.de/100MB.bin --output 100MB.bin
