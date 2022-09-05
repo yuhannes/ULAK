62d2af9bcd60b986a9d742224c50fb8864ec8185
yuhannes <112183271+yuhannes@users.noreply.github.com>
2022-09-05 05:04:10 +0300

    ULAK SYSTEM

--- ulaksystem/scripts/init.sh
+++ ulaksystem/scripts/init.sh
@@ -8,19 +8,26 @@ ENDCOLOR="\e[0m"
 # echo "UBUNTU APT"
 # apt-get update
 
+# OpenSSH
+echo "${RED}OPENSSH${ENDCOLOR}"
+apt-get -y install openssh-client
+echo "${GREEN}OPENSSH PACKAGE ... DONE${ENDCOLOR}"
+
 # RSYNC
-echo -e "${RED}RSYNC${ENDCOLOR}"
+echo "${RED}RSYNC${ENDCOLOR}"
 apt-get -y install rsync
+echo "${GREEN}RSYNC PACKAGE ... DONE${ENDCOLOR}"
 
 # CURL
 # echo "CURL"
 # apt-get -y install curl
 
 # TEST FILES
-echo -e "${RED}TEST FILES${ENDCOLOR}"
 mkdir data
 cd data/
+echo "${RED}TEST FILES${ENDCOLOR}"
 curl http://ipv4.download.thinkbroadband.com/20MB.zip --output 20MB.zip
 curl http://ipv4.download.thinkbroadband.com/50MB.zip --output 50MB.zip
 curl http://ipv4.download.thinkbroadband.com/100MB.zip --output 100MB.zip
 curl https://speed.hetzner.de/100MB.bin --output 100MB.bin
+echo "${GREEN}DOWNLOAD COMPLETED${ENDCOLOR}"
