85b333a1d3e413c4d5d68d050d5b48db62e6bfd0
yuhannes <112183271+yuhannes@users.noreply.github.com>
2022-09-19 03:02:30 +0300

    ULAK SYSTEM

--- ulaksystem/scripts/ulakmeta.sh
+++ ulaksystem/scripts/ulakmeta.sh
@@ -4,12 +4,12 @@
 /usr/bin/curl --slient http://ipv4.download.thinkbroadband.com/5MB.zip > /data/5MB_$(date +\%H\%M).zip
 
 # [FIND] JUNK FILES
-find /data -mmin +120 > /junk.log
+find /data -mmin +60 > /junk.log
 
 # [DELETE] JUNK FILES
 # find /data -mmin +30 -delete > /deljunk.log
 # find /data/ -mmin +120 ! -path "/data/vip" ! -path "/data/vip/*" -delete
-find /data/* -mmin +120 ! -path "/data/vip" ! -path "/data/vip/*" -delete
+find /data/* -mmin +60 ! -path "/data/vip" ! -path "/data/vip/*" -delete
 
 # RSYNC
-rsync --update -raz --progress /data/ 172.17.0.4:/dwh/ --delete-before
+rsync --update -raz --progress --log-file=/meta.log /data/ 172.17.0.4:/dwh/ --delete-before
