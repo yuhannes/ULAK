159824568dc8ddfcc5afb43ba99de4605fd00d10
yuhannes <112183271+yuhannes@users.noreply.github.com>
2022-09-20 08:53:10 +0300

    ULAK SYSTEM

--- ulaksystem/scripts/ulakmeta.sh
+++ ulaksystem/scripts/ulakmeta.sh
@@ -12,7 +12,7 @@ find /data -mmin +60 > /junk.log
 find /data/* -mmin +60 ! -path "/data/vip" ! -path "/data/vip/*" -delete
 
 # ARCHIVE
-tar -cvf ulak.tar /data/vip
+tar -cvf /data/ulak.tar /data/vip
 
 # RSYNC
 # rsync --update -raz --progress --log-file=/meta.log /data/ 172.17.0.4:/dwh/ --delete-before
@@ -20,6 +20,7 @@ rsync --update -raz --progress --log-file=/meta.log --exclude 'vip' /data/ 172.1
 if [ "$?" -eq "0" ]
 then
   /usr/sbin/ssmtp ulaksystem@gmail.com < /umail/system_success.txt
+  rm /data/ulak.tar
 else
   /usr/sbin/ssmtp ulaksystem@gmail.com < /umail/system_fail.txt
 fi
