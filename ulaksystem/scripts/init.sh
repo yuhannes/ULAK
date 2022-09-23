a28bee507473f7056890371d5f64f2c130b7479b
yuhannes <112183271+yuhannes@users.noreply.github.com>
2022-09-23 23:35:44 +0300

    ULAK SYSTEM

--- ulaksystem/scripts/init.sh
+++ ulaksystem/scripts/init.sh
@@ -52,6 +52,6 @@ cd /data/
 echo "${RED}TEST FILES${ENDCOLOR}"
 curl http://ipv4.download.thinkbroadband.com/20MB.zip --output 20MB.zip
 curl http://ipv4.download.thinkbroadband.com/50MB.zip --output 50MB.zip
-curl http://ipv4.download.thinkbroadband.com/100MB.zip --output 100MB.zip
-curl https://speed.hetzner.de/100MB.bin --output 100MB.bin
+# curl http://ipv4.download.thinkbroadband.com/100MB.zip --output 100MB.zip
+# curl https://speed.hetzner.de/100MB.bin --output 100MB.bin
 echo "${GREEN}DOWNLOAD COMPLETED${ENDCOLOR}"
